dcbd9eceed4ddb6bad9d94cc2536152bf1d0f7d0
Jason Ling <105073210+Jasonlingg@users.noreply.github.com>
2023-07-26 04:28:07 -0400

    Styling

--- content-translator/src/App.js
+++ content-translator/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import JokesContent from './JokesContent';
 function App() {
   return (
-    <div className="App">
+    <div className="">
           <JokesContent />
     </div>
   );
--- content-translator/src/JokesContent.js
+++ content-translator/src/JokesContent.js
@@ -1,51 +1,81 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function JokesContent(){
-    const fetchJokes = async () => { // try catch block
-        try {
-        const response = await axios.get(`https://v2.jokeapi.dev/joke/${selectedCategory}?type=single`);
-        return response.data.joke;
-        } catch (error) {
-        console.error('Error:', error);
-        return null;
-        }
-    };
+import './App.css';
 
-    const [joke, setJoke] = useState('');
-    const [selectedCategory, setSelectedCategory] = useState('Any'); // Default category: 'Any'
+function JokesContent() {
+  const [title, setTitle] = useState('');
+  const titleText = "Joke Generator"; // The text you want to show with typing effect
+  const typingSpeed = 100; // Speed of typing in milliseconds
 
-    const categories = ['Any', 'Misc', 'Programming', 'Dark', 'Pun'];
-  
-    useEffect(() => {
-      // Fetch jokes when the component mounts
-      fetchNewJoke();
-    }, []); // only runs once when component mounts
-
-
-    const handleCategoryChange = (event) => {
-      setSelectedCategory(event.target.value);
-    };
-    const fetchNewJoke = async () => {
-      const newJoke = await fetchJokes();
-      if (newJoke) {
-        setJoke(newJoke);
+  useEffect(() => {
+    // Simulate typing effect on the title
+    let currentIndex = 0;
+    const interval = setInterval(() => {
+      if (currentIndex <= titleText.length) {
+        setTitle(titleText.slice(0, currentIndex));
+        currentIndex++;
+      } else {
+        clearInterval(interval);
       }
-    };
-  
-return (
-    <div>
-      <h2>Joke of the Day</h2>
-      <select value={selectedCategory} onChange={handleCategoryChange}>
-        {categories.map((category) => (
-          <option key={category} value={category}>
-            {category}
-          </option>
-        ))}
-      </select>
-      <button onClick={fetchNewJoke}>GET JOKE LOL</button>
-      <p>{joke}</p>
+    }, typingSpeed);
+
+    return () => clearInterval(interval);
+  }, []);
+  const fetchJokes = async (selectedCategory) => {
+    try {
+      const response = await axios.get(`https://v2.jokeapi.dev/joke/${selectedCategory}?type=single`);
+      return response.data.joke;
+    } catch (error) {
+      console.error('Error:', error);
+      return null;
+    }
+  };
+
+  const [joke, setJoke] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('Any');
+
+  const categories = ['Any', 'Misc', 'Programming', 'Dark', 'Pun'];
+
+  useEffect(() => {
+    fetchNewJoke();
+  }, []);
+
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const fetchNewJoke = async () => {
+    const newJoke = await fetchJokes(selectedCategory);
+    if (newJoke) {
+      setJoke(newJoke);
+    }
+  };
+
+
+  return (
+    <div className="jokes-background">
+      <div className="jokes-container">
+      <div className='laugh'>&#128514;</div>
+        <h1 className="jokes-title">{title}<span className="cursor"></span></h1>
+        <div>
+          <p>Select</p>
+        <select className="jokes-select" value={selectedCategory} onChange={handleCategoryChange}>
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+        </div>
+
+        <div>
+          <button className="jokes-button" onClick={fetchNewJoke}>Generate Joke</button>
+        </div>
+        <p className="jokes-content">{joke}</p>
+      </div>
     </div>
   );
 }
+
 export default JokesContent;
